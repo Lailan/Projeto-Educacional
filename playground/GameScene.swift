b8d1f29cb2d3dc12630548c784b08370c4e07be0
Lailan Rogério <lailan@me.com>
2019-01-31 14:57:29 -0200

    Comit inicial

--- playground/GameScene.swift
+++ playground/GameScene.swift
@@ -10,101 +10,66 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+   
+    var objetoMovel = SKSpriteNode(color: UIColor.lightGray, size: CGSize(width: 140, height: 140))
+    var objetoFixo = SKSpriteNode(color: UIColor.orange, size: CGSize(width: 150, height: 150))
+    var objetoFake = SKSpriteNode(color: UIColor.orange, size: CGSize(width: 150, height: 150))
+   
+    var touchEncontrou = false
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
+    override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+       
+        objetoFixo.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        objetoFixo.position = CGPoint(x: 190, y: -300)
+        objetoFake.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        objetoFake.position = CGPoint(x: -190, y: 200)
+        objetoMovel.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        objetoMovel.position = CGPoint(x: 0, y: 0)
+       
+        self.addChild(objetoFixo)
+        self.addChild(objetoFake)
+        self.addChild(objetoMovel)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+       
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+       
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            objetoMovel.position.x = location.x
+            objetoMovel.position.y = location.y
+            validarSeElementosEncontraram(objetoMovel)
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+    }
+    
+    func validarSeElementosEncontraram(_ objetoMovel:SKSpriteNode){
+        let posicaoXfixa = objetoFixo.position.x
+        let posicaoYfixa = objetoFixo.position.y
+        if (objetoMovel.position.x > posicaoXfixa-10 ) && (objetoMovel.position.x < posicaoXfixa+10) && (objetoMovel.position.y > posicaoYfixa-10 ) && (objetoMovel.position.y < posicaoYfixa+10){
+            objetoMovel.color = UIColor.green
+            if !(touchEncontrou) {
+                vibrarTela()
+                touchEncontrou = true
+            }
+        } else {
+            objetoMovel.color = UIColor.lightGray
+            if touchEncontrou {
+               touchEncontrou = false
+            }
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+    }
+   
+    func vibrarTela(){
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
